c2369b79e9a38c9e2a8abf11082167bd331a671e
Lucian Iernye <lucian.iernye@procode.co.uk>
2024-12-03 08:51:47 -0500

    run nosetest and fix the factories.py

--- tests/factories.py
+++ tests/factories.py
@@ -20,7 +20,6 @@ Test Factory to make fake objects for testing
 import factory
 from factory.fuzzy import FuzzyChoice, FuzzyDecimal
 from service.models import Product, Category
-from factory.faker import faker
 
 
 class ProductFactory(factory.Factory):
@@ -31,17 +30,31 @@ class ProductFactory(factory.Factory):
 
         model = Product
 
-    FAKE = faker.Faker()
     id = factory.Sequence(lambda n: n)
-    name = FAKE.name()
-    description = FAKE.text()
-    price = FuzzyDecimal(0.5, 1000, 100)
-    available = FuzzyChoice(choices=[True, False])
+    name = FuzzyChoice(
+        choices=[
+        "Hat", 
+        "Pants", 
+        "Apple", 
+        "Shirt", 
+        "Pots", 
+        "Towels", 
+        "Ford", 
+        "Chevy", 
+        "Hammer", 
+        "Wrench"
+        ]
+    )
+    description = factory.Faker("text")
+    price = FuzzyDecimal(0.5, 2000.0, 2)    
+    available = FuzzyChoice(choices = [True, False])
     category = FuzzyChoice(
         choices = [
-            Category.FOOD,
+            Category.UNKNOWN,
             Category.CLOTHS,
-            Category.TOOLS,
-            Category.AUTOMOTIVE
+            Category.FOOD,
+            Category.HOUSEWARES,
+            Category.AUTOMOTIVE,
+            Category.TOOLS
         ]
     )
